56086b7f9b59497712812f0e2e767933afbc75a6
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-21 05:50:36 +0000

    #2 Add repos

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
+repo_url = 'https://api.github.com/users/mezgoodle/repos?sort=pushed'
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    repos = requests.get(repo_url).json()[:9]
+    return render_template('index.html', repos=repos)
 
 if __name__ == '__main__':
     app.run(debug=True)
