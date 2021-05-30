41d3d03a5afc432459d6d67351821bf245d1a0c3
Maxim Zavalniuk <mezidiaofficial@gmail.com>
2021-05-30 07:12:41 +0000

    Add some components

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,18 +1,39 @@
 <template>
   <div>
     <Header />
-    <Main />
+    <Profiles />
+    <Loader v-if="loading"/>
+    <Main 
+      v-else-if="repos.length"
+      v-bind:repos="repos"
+    />
+    <div v-else class="container alert alert-danger text-center" role="alert">
+      No projects
+    </div>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
+import Loader from '@/components/Loader.vue'
+import Profiles from '@/components/Profiles.vue'
 import Main from '@/components/Main.vue'
 
 export default {
   name: 'App',
+  data () {
+    return {
+      repos: [],
+      loading: true
+    }
+  },
+  async mounted () {
+    const res = await fetch('https://api.github.com/users/mezgoodle/repos?sort=updated');
+    const repos = await res.json();
+    this.repos = repos;
+  },
   components: {
-    Header, Main
+    Header, Main, Loader, Profiles
   }
 }
 </script>
--- portfolio/src/components/Main.vue
+++ portfolio/src/components/Main.vue
@@ -1,17 +1,5 @@
 <template>
   <main>
-    <section class="py-5 text-center container">
-        <div class="row py-lg-5">
-          <div class="col-lg-6 col-md-8 mx-auto">
-              <h1 class="font-weight-light">Welcome to my minimal portfolio!</h1>
-              <p class="lead text-muted">Hello! My name is Maxim Zavalniuk and I am from Kyiv, Ukraine. I like to learn new things,share them and help other people. I am always looking for new interesting work. And I love GitHub. 😊</p>
-              <p>
-                <a href="https://github.com/mezgoodle" title="GitHub" class="btn btn-dark mr-2">GitHub profile</a>
-                <a href="https://gitlab.com/mezgoodle" title="GitLab" class="btn btn-warning">GitLab profile</a>
-              </p>
-          </div>
-        </div>
-    </section>
     <div class="album py-5 bg-light">
         <figure class="text-center">
           <blockquote class="blockquote">
