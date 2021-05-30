507f173596626cfed4252adeee579c93f0dc16d8
Maxim Zavalniuk <mezidiaofficial@gmail.com>
2021-05-30 07:28:01 +0000

    Some updates

--- portfolio/src/components/Main.vue
+++ portfolio/src/components/Main.vue
@@ -11,177 +11,10 @@
         </figure>
         <div class="container">
           <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-              <div class="col">
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: Templates" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                      <title>Templates</title>
-                      <rect width="100%" height="100%" fill="#55595c"/>
-                      <text x="50%" y="50%" fill="#eceeef" dy=".3em">Templates</text>
-                    </svg>
-                    <div class="card-body">
-                      <p class="card-text">📚I create this repository to help me create useful files in every my project. This is my style of developing🔖 </p>
-                      <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                            <a href="https://github.com/mezgoodle/Templates" class="pr-1" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                            <a href="https://github.com/mezgoodle/Templates/archive/master.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
-                          </div>
-                          <small class="text-muted">2 stars</small>
-                      </div>
-                    </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: portfolio" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                      <title>portfolio</title>
-                      <rect width="100%" height="100%" fill="#55595c"/>
-                      <text x="50%" y="50%" fill="#eceeef" dy=".3em">portfolio</text>
-                    </svg>
-                    <div class="card-body">
-                      <p class="card-text">My portfolio site😊</p>
-                      <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                            <a href="https://github.com/mezgoodle/portfolio" class="pr-1" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                            <a href="https://github.com/mezgoodle/portfolio/archive/master.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
-                          </div>
-                          <small class="text-muted">3 stars</small>
-                      </div>
-                    </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: numericalMethods_labs" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                      <title>numericalMethods_labs</title>
-                      <rect width="100%" height="100%" fill="#55595c"/>
-                      <text x="50%" y="50%" fill="#eceeef" dy=".3em">numericalMethods_labs</text>
-                    </svg>
-                    <div class="card-body">
-                      <p class="card-text">My labs for numerical methods in KPI🧮</p>
-                      <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                            <a href="https://github.com/mezgoodle/numericalMethods_labs" class="pr-1" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                            <a href="https://github.com/mezgoodle/numericalMethods_labs/archive/master.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
-                          </div>
-                          <small class="text-muted">1 star</small>
-                      </div>
-                    </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: mezgoodle" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                      <title>mezgoodle</title>
-                      <rect width="100%" height="100%" fill="#55595c"/>
-                      <text x="50%" y="50%" fill="#eceeef" dy=".3em">mezgoodle</text>
-                    </svg>
-                    <div class="card-body">
-                      <p class="card-text">My ✨special ✨ repository</p>
-                      <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                            <a href="https://github.com/mezgoodle/mezgoodle" class="pr-1" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                            <a href="https://github.com/mezgoodle/mezgoodle/archive/master.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
-                          </div>
-                          <small class="text-muted">2 stars</small>
-                      </div>
-                    </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: TestLabs-kpi" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                      <title>TestLabs-kpi</title>
-                      <rect width="100%" height="100%" fill="#55595c"/>
-                      <text x="50%" y="50%" fill="#eceeef" dy=".3em">TestLabs-kpi</text>
-                    </svg>
-                    <div class="card-body">
-                      <p class="card-text">My labs for testing software in KPI😊</p>
-                      <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                            <a href="https://github.com/mezgoodle/TestLabs-kpi" class="pr-1" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                            <a href="https://github.com/mezgoodle/TestLabs-kpi/archive/master.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
-                          </div>
-                          <small class="text-muted">1 star</small>
-                      </div>
-                    </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: SQL" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                      <title>SQL</title>
-                      <rect width="100%" height="100%" fill="#55595c"/>
-                      <text x="50%" y="50%" fill="#eceeef" dy=".3em">SQL</text>
-                    </svg>
-                    <div class="card-body">
-                      <p class="card-text">Repository about SQL, but also about experiments🤪</p>
-                      <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                            <a href="https://github.com/mezgoodle/SQL" class="pr-1" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                            <a href="https://github.com/mezgoodle/SQL/archive/master.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
-                          </div>
-                          <small class="text-muted">1 star</small>
-                      </div>
-                    </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: PyContacts" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                      <title>PyContacts</title>
-                      <rect width="100%" height="100%" fill="#55595c"/>
-                      <text x="50%" y="50%" fill="#eceeef" dy=".3em">PyContacts</text>
-                    </svg>
-                    <div class="card-body">
-                      <p class="card-text">Contact book on PyQt5 and MongoDB📇</p>
-                      <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                            <a href="https://github.com/mezgoodle/PyContacts" class="pr-1" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                            <a href="https://github.com/mezgoodle/PyContacts/archive/master.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
-                          </div>
-                          <small class="text-muted">2 stars</small>
-                      </div>
-                    </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: breaker" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                      <title>breaker</title>
-                      <rect width="100%" height="100%" fill="#55595c"/>
-                      <text x="50%" y="50%" fill="#eceeef" dy=".3em">breaker</text>
-                    </svg>
-                    <div class="card-body">
-                      <p class="card-text">Monitors your working time. Built with Electron😊</p>
-                      <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                            <a href="https://github.com/mezgoodle/breaker" class="pr-1" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                            <a href="https://github.com/mezgoodle/breaker/archive/master.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
-                          </div>
-                          <small class="text-muted">3 stars</small>
-                      </div>
-                    </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: Caesar-and-Vigenere-ciphers" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                      <title>Caesar-and-Vigenere-ciphers</title>
-                      <rect width="100%" height="100%" fill="#55595c"/>
-                      <text x="50%" y="50%" fill="#eceeef" dy=".3em">Caesar-and-Vigenere-ciphers</text>
-                    </svg>
-                    <div class="card-body">
-                      <p class="card-text">🔐This is the repository of my project on topic: &#34;Caesar and Vigenere ciphers&#34;</p>
-                      <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                            <a href="https://github.com/mezgoodle/Caesar-and-Vigenere-ciphers" class="pr-1" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                            <a href="https://github.com/mezgoodle/Caesar-and-Vigenere-ciphers/archive/master.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
-                          </div>
-                          <small class="text-muted">3 stars</small>
-                      </div>
-                    </div>
-                </div>
-              </div>
+              <RepoCard 
+                v-for="repo of repos" :key="repo"
+                v-bind:repo="repo"
+              />
           </div>
         </div>
     </div>
@@ -189,24 +22,13 @@
 </template>
 
 <script>
+import RepoCard from '@/components/RepoCard.vue'
+
 export default {
-  name: 'Main'
+  name: 'Main',
+  props: ['repos'],
+  components: {
+    RepoCard
+  }
 }
 </script>
-
-<style scoped>
-  .bd-placeholder-img {
-    font-size: 1.125rem;
-    text-anchor: middle;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-  }
-
-  @media (min-width: 768px) {
-    .bd-placeholder-img-lg {
-      font-size: 3.5rem;
-    }
-  }
-</style>
---
+++ portfolio/src/components/RepoCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="col">
+    <div class="card shadow-sm">
+        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: Templates" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
+          <title>Templates</title>
+          <rect width="100%" height="100%" fill="#55595c"/>
+          <text x="50%" y="50%" fill="#eceeef" dy=".3em">Templates</text>
+        </svg>
+        <div class="card-body">
+          <p class="card-text">📚I create this repository to help me create useful files in every my project. This is my style of developing🔖 </p>
+          <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <a href="https://github.com/mezgoodle/Templates" class="pr-1" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
+                <a href="https://github.com/mezgoodle/Templates/archive/master.zip"><button type="button" class="btn btn-sm btn-outline-secondary">Download</button></a>
+              </div>
+              <small class="text-muted">2 stars</small>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RepoCard',
+  props: {
+    repo: {
+      type: Object,
+      required: true
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .bd-placeholder-img {
+    font-size: 1.125rem;
+    text-anchor: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  @media (min-width: 768px) {
+    .bd-placeholder-img-lg {
+      font-size: 3.5rem;
+    }
+  }
+</style>
