19bccca339937e3a9db49cc6638be1e3b3db3ba9
Maksym Zavalniuk <mezgoodle@gmail.com>
2024-02-12 12:48:47 +0200

    Create gatsby project

---
+++ portfolio/gatsby-config.js
@@ -0,0 +1,40 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "",
+        spaceId: "4grgjgxotvn2",
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
