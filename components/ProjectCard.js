bd989a4527106fa37fe7a9a57a494147a99e4a73
Maksym Zavalniuk <mezgoodle@gmail.com>
2024-02-18 15:49:16 +0200

    Add Images

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,10 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function ProjectCard({ project }) {
   const { title, slug, cookingTime, thumbnail } = project.fields;
   return (
     <div className="card">
-      <div className="featured">{/* image */}</div>
+      <div className="featured">
+        <Image
+          src={"https:" + thumbnail.fields.file.url}
+          alt={title}
+          width={thumbnail.fields.file.details.image.width}
+          height={thumbnail.fields.file.details.image.height}
+        />
+      </div>
       <div className="content">
         <div className="info">
           <h4>{title}</h4>
---
+++ next.config.js
@@ -0,0 +1,14 @@
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
