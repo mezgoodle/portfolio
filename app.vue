958745a92d31afcc15a6d84e99477818bdb228c5
Maksym Zavalniuk <mezgoodle@gmail.com>
2025-03-09 22:25:36 +0200

    Get data from supabase

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
+<script setup>
+import { createClient } from "@supabase/supabase-js";
+
+const config = useRuntimeConfig();
+const supabase = createClient(
+  config.public.supabaseUrl,
+  config.public.supabaseKey
+);
+const projects = ref([]);
+
+async function getProjects() {
+  const { data } = await supabase.from("projects").select();
+  projects.value = data;
+}
+
+onMounted(() => {
+  getProjects();
+});
+</script>
+
 <template>
   <NuxtLayout>
+    <h1>Проєкти</h1>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.title }}
+      </li>
+    </ul>
     <NuxtPage />
   </NuxtLayout>
 </template>
