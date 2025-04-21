a60577634cec80a4f942bfbc4f2a242adac82693
Maksym Zavalniuk <mezgoodle@gmail.com>
2025-04-21 11:36:05 +0300

    Init the project

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-light py-3 text-center fixed-bottom">
-      <p>© {new Date().getFullYear()} Maksym Zavalniuk</p>
-    </footer>
-  );
-};
-
-export default Footer;
--- lib/interfaces/Hobby.tsx
+++
@@ -1,7 +0,0 @@
-interface Hobby {
-  name: string;
-  description?: string; // Optional description
-  icon?: string; // Optional icon URL or class name
-}
-
-export default Hobby;
